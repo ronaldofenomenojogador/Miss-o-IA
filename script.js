20bbadb470bae309e218fbe5c15cbeeab9692248
André <115079125+ronaldofenomenojogador@users.noreply.github.com>
2024-08-14 07:28:02 -0300

    h

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    enunciado: "Pergunta 1",
-    alternativas:
-
-    "Pergunta 2"
-]
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"]
+    }
+];
